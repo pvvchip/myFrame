ee5fadf63ef0c478d8e54a0dba11c8d004454cb4
Vladimir P <pvvchip@gmail.com>
2022-06-30 14:00:19 +0300

    Initial commit

---
+++ src/main/java/ru/pvvchip/myframe/MyFrameApplication.java
@@ -0,0 +1,13 @@
+package ru.pvvchip.myframe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFrameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyFrameApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/pvvchip/myframe/MyFrameApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.pvvchip.myframe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFrameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
